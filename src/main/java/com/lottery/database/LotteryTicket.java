ec5da114e74caeee210efab1a1318c6f1fee3731
Wint-1234 <winton.ebanks@yahoo.com>
2022-10-03 16:47:37 -0400

    .

--- src/main/java/com/lottery/database/LotteryTicket.java
+++ src/main/java/com/lottery/database/LotteryTicket.java
@@ -1,36 +1,30 @@
 package com.lottery.database;
 
-<<<<<<< HEAD
-public class LotteryTicket {
-
-
-
-
-=======
 import java.sql.Date;
 
-public class LotteryTicket implements Comparable<LotteryTicket> {
-  private final Date date;
-  private final int[] lotteryNumbers;
-  private final int megaBallNumber;
+  public class LotteryTicket implements Comparable<LotteryTicket> {
 
-  public LotteryTicket(Date date, int[] lotteryNumbers, int megaBallNumber){
-    this.date = date;
-    this.lotteryNumbers = lotteryNumbers;
-    this.megaBallNumber = megaBallNumber;
-  }
+    private final Date date;
+    private final int[] lotteryNumbers;
+    private final int megaBallNumber;
 
-  @Override
-  public int compareTo(LotteryTicket other) {
-    return this.date.compareTo(other.date);
-  }
+    public LotteryTicket(Date date, int[] lotteryNumbers, int megaBallNumber) {
+      this.date = date;
+      this.lotteryNumbers = lotteryNumbers;
+      this.megaBallNumber = megaBallNumber;
+    }
 
-  public int[] getLotteryNumbers() {
-    return lotteryNumbers;
-  }
+    @Override
+    public int compareTo(LotteryTicket other) {
+      return this.date.compareTo(other.date);
+    }
 
-  public int getMegaBallNumber() {
-    return megaBallNumber;
+    public int[] getLotteryNumbers() {
+      return lotteryNumbers;
+    }
+
+    public int getMegaBallNumber() {
+      return megaBallNumber;
+    }
+
   }
->>>>>>> main
-}
