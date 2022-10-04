6910cb3ebfe04e1adc6608d9b6460be0931fe2ad
ejbanda <bandaericj@gmail.com>
2022-10-04 11:41:05 -0500

    small main class change. throws exception

--- src/main/java/com/lottery/megamillions/MegaMillions.java
+++ src/main/java/com/lottery/megamillions/MegaMillions.java
@@ -8,13 +8,10 @@ public class MegaMillions {
   public static final int EXIT_CODE = 0;
   public static final int MENU_CHOICE = 99;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
     int choice = MENU_CHOICE;
-    try {
-      MegaMillionsDatabase.readFile();
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    }
+    var database = new MegaMillionsDatabase().getLotteryTickets();
+
     Menu menu = new Menu();
     while (choice != EXIT_CODE){
       switch (choice){
@@ -24,7 +21,7 @@ public class MegaMillions {
         default:
           choice = menu.runMenu();
       }
-      //choice = menu.runMenu(); // runs the main menu and asks the user for input. change this?
+      choice = menu.runMenu(); // runs the main menu and asks the user for input. change this?
     }
 
 
