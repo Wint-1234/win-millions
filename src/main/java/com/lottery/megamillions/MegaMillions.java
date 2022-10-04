1896b9ce357144e083bb902a6ee3183d7848df1c
ejbanda <bandaericj@gmail.com>
2022-10-04 09:20:45 -0500

    Adjust classes so MegaMillions is the main.
    adjust toString in LotteryTicket

--- src/main/java/com/lottery/megamillions/MegaMillions.java
+++ src/main/java/com/lottery/megamillions/MegaMillions.java
@@ -1,6 +1,8 @@
 package com.lottery.megamillions;
 
+import com.lottery.database.MegaMillionsDatabase;
 import com.lottery.megamillions.controller.Menu;
+import java.io.FileNotFoundException;
 
 public class MegaMillions {
   public static final int EXIT_CODE = 0;
@@ -8,6 +10,11 @@ public class MegaMillions {
 
   public static void main(String[] args) {
     int choice = NEUTRAL_CHOICE_VALUE;
+    try {
+      MegaMillionsDatabase.readFile();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
     Menu menu = new Menu();
     while (choice != EXIT_CODE){
       choice = menu.runMenu(); // runs the main menu and asks the user for input. change this?
