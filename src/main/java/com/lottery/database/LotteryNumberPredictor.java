74411d14a5eea2c311154b1a9932df1a797f0d50
Wint-1234 <winton.ebanks@yahoo.com>
2022-10-04 15:22:01 -0400

    Added constraints to methods.

--- src/main/java/com/lottery/database/LotteryNumberPredictor.java
+++ src/main/java/com/lottery/database/LotteryNumberPredictor.java
@@ -1,7 +1,6 @@
 package com.lottery.database;
 
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,6 +13,9 @@ public class LotteryNumberPredictor {
   }
 
   public List<LotteryTicket> findByMonth(int month) {
+    if (month < 0 || month > 11) {
+      throw new IllegalArgumentException();
+    }
     return database
         .stream()
         .filter(lotteryTicket -> lotteryTicket.getDate().getMonth() == month)
@@ -21,6 +23,9 @@ public class LotteryNumberPredictor {
   }
 
   public List<LotteryTicket> findByYear(int year) {
+    if (year < 2017 || year > 2022) {
+      throw new IllegalArgumentException();
+    }
     return database
         .stream()
         .filter(lotteryTicket -> lotteryTicket.getDate().getYear() == year)
@@ -28,6 +33,9 @@ public class LotteryNumberPredictor {
   }
 
   public List<LotteryTicket> findByNumbers(int[] numbers) {
+    if (numbers == null) {
+      throw new NullPointerException();
+    }
     return database
         .stream()
         .filter(lotteryTicket -> containsNumbers(lotteryTicket.getLotteryNumbers(), numbers))
@@ -45,6 +53,9 @@ public class LotteryNumberPredictor {
   }
 
   private boolean containsNumbers(int[] numberArray, int[] numbersToCheck) {
+    if (numberArray == null || numbersToCheck == null) {
+      throw new NullPointerException();
+    }
     var listArray = List.of(numberArray);
     for (int value: numbersToCheck) {
       if (!listArray.contains(value)) {
