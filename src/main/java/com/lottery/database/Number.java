8df90efe1bc3be971df63bc10d68ce168e577637
Wint-1234 <winton.ebanks@yahoo.com>
2022-10-06 08:37:07 -0400

    Finalized Javadocs for Number

--- src/main/java/com/lottery/database/Number.java
+++ src/main/java/com/lottery/database/Number.java
@@ -1,31 +1,51 @@
 package com.lottery.database;
 
+/**
+ * Custom data obj to hold different numbers that could
+ * potentially hold the same count value.
+ *
+ * @version 1.0
+ */
 public class Number implements Comparable<Number> {
-  int number;
-  int count;
+  private int number;
+  private int count;
 
+  /**
+   * Initializes the field of this obj.
+   *
+   * @param number The number of this obj.
+   * @param count The frequency count of this number.
+   */
   public Number(int number, int count) {
     this.number = number;
     this.count = count;
   }
 
+  /**
+   * Compares two Number objects based on their count.
+   *
+   * @param other The object to be compared.
+   * @return int representation of comparing two Numbers based on count.
+   */
   @Override
   public int compareTo(Number other) {
-    int result = 0;
-    if (this.count == other.count) {
-      result = 0;
-    } else if (this.count > other.count) {
-      result = 1;
-    } else {
-      result = -1;
-    }
-    return result;
+    return Integer.compare(this.count, other.count);
   }
 
+  /**
+   * Gets the number of this obj.
+   *
+   * @return int of this obj.
+   */
   public int getNumber() {
     return number;
   }
 
+  /**
+   * Gets the count of this obj.
+   *
+   * @return int count of this number obj.
+   */
   public int getCount() {
     return count;
   }
