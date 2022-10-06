a477390a102aa32816ac652b75baa5c275a3a05d
Wint-1234 <winton.ebanks@yahoo.com>
2022-10-06 07:48:15 -0400

    Refactored constants in setLotteryNumbers(). Also added checkForDuplicates().

--- src/main/java/com/lottery/database/LotteryTicket.java
+++ src/main/java/com/lottery/database/LotteryTicket.java
@@ -2,7 +2,14 @@ package com.lottery.database;
 
 import java.sql.Date;
 import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
 
+/**
+ * Represents a new LotteryTicket based on date, lottery numbers, and a mega ball number
+ *
+ * @version 1.0
+ */
 public class LotteryTicket implements Comparable<LotteryTicket> {
 
   // Constants
@@ -11,27 +18,55 @@ public class LotteryTicket implements Comparable<LotteryTicket> {
   public static final int LOTTERY_NUMBERS_LOWER_BOUND = 1;
   public static final int LOTTERY_NUMBERS_UPPER_BOUND = 75;
   public static final String MEGA_BALL_ERROR = "Invalid Mega Ball number.";
+  public static final String DUPLICATES_ERROR = "Cannot create a lottery ticket with a"
+      + " duplicate number: ";
+  public static final int ARRAY_LENGTH_RESTRICTION = 5;
 
   private final Date date;
   private int[] lotteryNumbers;
   private int megaBallNumber;
 
+  /**
+   * Creates a new LotteryTicket. Sets the default date of this ticket to
+   * current date.
+   *
+   * @param lotteryNumbers Int array of five numbers from 1-70 (inclusive).
+   * @param megaBallNumber Int for mega ball number from 1-25 (inclusive).
+   */
   public LotteryTicket(int[] lotteryNumbers, int megaBallNumber) {
     this.date = new Date(System.currentTimeMillis());
     setLotteryNumbers(lotteryNumbers);
     setMegaBallNumber(megaBallNumber);
   }
 
+  /**
+   * Creates a new LotteryTicket. Sets the date of this ticket to the date
+   * passed in.
+   *
+   * @param date The date this ticket was drawn.
+   * @param lotteryNumbers Int array of five numbers from 1-70 (inclusive).
+   * @param megaBallNumber Int for mega ball number from 1-25 (inclusive).
+   */
   public LotteryTicket(Date date, int[] lotteryNumbers, int megaBallNumber) {
     this.date = date;
     setLotteryNumbers(lotteryNumbers);
     setMegaBallNumber(megaBallNumber);
   }
 
+  /**
+   * Checks constraints in the lottery numbers array of this ticket.
+   * If the array is null, does not have exactly five numbers from 1-70 (inclusive),
+   * or has duplicate numbers, an exception is thrown.
+   * Otherwise, it sets the lottery numbers of this ticket.
+   *
+   * @param lotteryNumbers
+   * @throws IllegalArgumentException
+   */
   public void setLotteryNumbers(int[] lotteryNumbers) {
-    if (lotteryNumbers.length != 5) {
+    if (lotteryNumbers == null || lotteryNumbers.length != ARRAY_LENGTH_RESTRICTION) {
       throw new IllegalArgumentException(CHOOSE_5_ERROR);
     }
+    checkForDuplicates(lotteryNumbers);        // Ensure lottery numbers does not contain duplicates
     Arrays.sort(lotteryNumbers);
     for (int number : lotteryNumbers) {
       if (number < LOTTERY_NUMBERS_LOWER_BOUND || number > LOTTERY_NUMBERS_UPPER_BOUND) {
@@ -68,5 +103,14 @@ public class LotteryTicket implements Comparable<LotteryTicket> {
   @Override
   public String toString(){
     return Arrays.toString(getLotteryNumbers()) + " (" + getMegaBallNumber() + ")\n";
+  }
+
+  private void checkForDuplicates(int[] lotteryNumbers) {
+    Set<Integer> set = new TreeSet<>();
+    for (int value: lotteryNumbers) {
+      if ((set.add(value) == false)) {
+        throw new IllegalArgumentException(DUPLICATES_ERROR + value);
+      }
+    }
   }
 }
